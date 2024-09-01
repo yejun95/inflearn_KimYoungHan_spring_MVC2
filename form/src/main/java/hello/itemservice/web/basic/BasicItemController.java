19e1fd238a59169ff19380e2dd0a3b8605d4b9e2
xodlf100 <xodlf100@gmail.com>
2024-09-01 22:39:10 +0900

    form directory 생성 및 체크박스 추가 * th:object * th:field

---
+++ form/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,87 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "basic/addForm";
+    }
+
+//    @PostMapping("add")
+//    public String saveV1(@RequestParam String itemName,
+//                       @RequestParam Integer price,
+//                       @RequestParam Integer quantity,
+//                       Model model) {
+//
+//        Item item = new Item(itemName, price, quantity);
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//        return "basic/item";
+//    }
+
+    @PostMapping("add")
+    public String saveV2(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+        log.info("item.open={}", item.getOpen());
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId()); // redirect 하면서 pathvariable 넘기기
+        redirectAttributes.addAttribute("status", true); // url 뒤에 쿼리파라미터로 붙음, ex) ?status=true
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    // @ModelAttribue : Param값을 객체로 맵핑
+    @PostMapping("/{itemId}/edit")
+    public String update(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}"; // PRG 패턴 Post-Redirect-Get
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
