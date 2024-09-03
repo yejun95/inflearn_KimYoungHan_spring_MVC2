204dd4cba100710c1f50d1ff8bb969977f86ebf4
xodlf100 <xodlf100@gmail.com>
2024-09-04 01:08:51 +0900

    국제화 테스트

---
+++ message/src/test/java/hello/itemservice/domain/message/MessageSourceTest.java
@@ -0,0 +1,44 @@
+package hello.itemservice.domain.message;
+
+import org.assertj.core.api.Assertions;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void argumentMessage() {
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void englishTest() {
+        String hello = ms.getMessage("hello", null, Locale.ENGLISH);
+        assertThat(hello).isEqualTo("hello");
+    }
+}
