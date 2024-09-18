a4fbbd9ff540d500ad417c7b2ba22335e453aaed
xodlf100 <xodlf100@gmail.com>
2024-09-18 20:46:15 +0900

    add spring interceptor log

---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,51 @@
+package hello.login.web.interceptor;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        // afterCompletion에 사용하기 위해 여기서 선언
+        // 전역변수로 선언하면 싱글톤이 되버리므로, 다른 유저 접근 시 값이 변해버림
+        request.setAttribute(LOG_ID, uuid);
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;// 호출할 컨트로러의 메서드의 모든 정보가 포함
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+
+        // 예외를 여기 찍는 이유는 postHandle이 호출되지 않는 상황이 있기 때문
+        log.info("REQUEST [{}][{}][{}]", logId, requestURI, handler);
+        if (ex != null)
+            log.error("afterCompletion error!!", ex);
+    }
+}
