285a1a1b81bdd39ad5fd079a44899ef6bbd2506c
xodlf100 <xodlf100@gmail.com>
2024-09-21 21:46:50 +0900

    MediaTyep에 따른 api exception 처리

--- exception/src/main/java/hello/exception/WebServerCostomizer.java
+++ exception/src/main/java/hello/exception/WebServerCostomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCostomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
