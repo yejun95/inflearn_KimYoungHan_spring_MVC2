c4f88ae22b70774e50bfbba565c3ea694bed8790
xodlf100 <xodlf100@gmail.com>
2024-09-24 00:32:35 +0900

    add WebConfig에 converter 추가

--- typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,8 +1,11 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -14,6 +17,19 @@ public class HelloController {
         String data = request.getParameter("data");
         Integer intValue = Integer.valueOf(data);
         System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("data = {}", data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP =" + ipPort.getIp());
+        System.out.println("ipPort Port =" + ipPort.getPort());
         return "ok";
     }
 }
